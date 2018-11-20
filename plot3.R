7d879bead2c15cfcf3e7b2da8771442c24ebb7b3
AbMani <abhinavmani08@gmail.com>
2018-11-21 01:18:01 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,19 @@
+
+  hpc<-read.table("C:\\Users\\61710499\\Desktop/Coursera - EDA\\Week 1\\exdata%2Fdata%2Fhousehold_power_consumption\\household_power_consumption.txt",sep = ";",stringsAsFactors = F)
+  hpc_colnames<-as.character(hpc[1,])
+  colnames(hpc)<-hpc_colnames
+  hpc<-hpc[-1,]
+  hpc$Date<-as.Date(hpc$Date,format = "%d/%m/%Y")
+  hpc$Time<-format(strptime(hpc$Time,format = "%H:%M:%S"),format = "%H:%M:%S")
+  hpc<-with(hpc,hpc[(Date>= "2007-02-01" & Date<="2007-02-02"),])
+  str(hpc)
+  summary(hpc)
+ 
+  
+  #plot 3
+  png("plot3.png",width = 480,height = 480)
+  plot (hpc$DateTime,as.numeric(hpc$Sub_metering_1),type = "l", xlab = "",ylab = "Energy Sub Metering")
+  lines(hpc$DateTime,as.numeric(hpc$Sub_metering_2),type = "l",col = "red")
+  lines(hpc$DateTime,as.numeric(hpc$Sub_metering_3),type = "l",col = "blue")
+  legend("topright",legend = colnames(hpc[,7:9]),col=c("black","red","blue"),lty=1,cex = 0.8)
+  dev.off()
